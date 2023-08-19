2e6c677a4ce4e8e0723e5f8b4537d9d8f41dd1e4
brianajiks123 <brianajiks123@gmail.com>
2023-08-19 17:04:09 +0700

    add base configuration to deploy in github

--- vite.config.js
+++ vite.config.js
@@ -5,14 +5,13 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
+  base: '/vue-todolist-composition/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
       '@comp': fileURLToPath(new URL('./src/components', import.meta.url)),
-      '@store': fileURLToPath(new URL('./src/stores', import.meta.url)),
+      '@store': fileURLToPath(new URL('./src/stores', import.meta.url))
     }
   }
 })
